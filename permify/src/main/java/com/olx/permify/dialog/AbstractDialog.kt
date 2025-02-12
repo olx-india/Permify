3aec6321604347c77591928414e8c2b6597a63dd
Abhishek Pandey <abhishek.pandey@olx.in>
2025-02-12 19:27:34 +0530

    [OLXIN-5870] : Resolved comments

--- permify/src/main/java/com/olx/permify/dialog/AbstractDialog.kt
+++ permify/src/main/java/com/olx/permify/dialog/AbstractDialog.kt
@@ -2,7 +2,6 @@ package com.olx.permify.dialog
 
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.view.View
 
 abstract class AbstractDialog(context: Context, themeResId: Int) :
