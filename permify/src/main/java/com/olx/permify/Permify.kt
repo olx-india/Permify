dea7a2048c652eac8ee6c7425dfbcb5dfd80ce40
Abhishek Pandey <abhishek.pandey@olx.in>
2025-02-12 21:03:43 +0530

    [OLXIN-5870] : Resolved comments

--- permify/src/main/java/com/olx/permify/Permify.kt
+++ permify/src/main/java/com/olx/permify/Permify.kt
@@ -3,6 +3,7 @@ package com.olx.permify
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.olx.permify.callback.PermissionCallback
+import com.olx.permify.utils.Logger
 import com.olx.permify.utils.OPEN_SETTING_MESSAGE
 import com.olx.permify.utils.REQUEST_MESSAGE
 import java.lang.ref.WeakReference
@@ -14,8 +15,10 @@ object Permify {
         permissions: List<String>,
         permissionCallback: PermissionCallback,
         requestMessage: String = REQUEST_MESSAGE,
-        openSettingMessage: String = OPEN_SETTING_MESSAGE
+        openSettingMessage: String = OPEN_SETTING_MESSAGE,
+        enableLogs: Boolean = false
     ) {
+        Logger.debug = enableLogs
         val weakActivity = WeakReference(activity)
         PermissionRequestBuilder(weakActivity, null, permissions)
             .setPermissionRequestMessages(requestMessage, openSettingMessage)
@@ -29,8 +32,10 @@ object Permify {
         permissions: List<String>,
         permissionCallback: PermissionCallback,
         requestMessage: String = REQUEST_MESSAGE,
-        openSettingMessage: String = OPEN_SETTING_MESSAGE
+        openSettingMessage: String = OPEN_SETTING_MESSAGE,
+        enableLogs: Boolean = false
     ) {
+        Logger.debug = enableLogs
         val weakActivity = WeakReference(fragment.requireActivity())
         val weakFragment = WeakReference(fragment)
         return PermissionRequestBuilder(
