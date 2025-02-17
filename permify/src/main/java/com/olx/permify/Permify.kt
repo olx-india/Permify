a22fd8f750b48be24fd4676412add77eb337f36f
Abhishek Pandey <abhishek.pandey@olx.in>
2025-02-17 11:12:55 +0530

    [OLXIN-5870] : Resolved comments

--- permify/src/main/java/com/olx/permify/Permify.kt
+++ permify/src/main/java/com/olx/permify/Permify.kt
@@ -1,7 +1,13 @@
 package com.olx.permify
 
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.olx.permify.callback.DefaultPermissionCallbackImpl
 import com.olx.permify.callback.PermissionCallback
 import com.olx.permify.utils.Logger
 import com.olx.permify.utils.OPEN_SETTING_MESSAGE
@@ -10,39 +16,65 @@ import java.lang.ref.WeakReference
 
 object Permify {
 
+    @JvmOverloads
     fun requestPermission(
         activity: FragmentActivity,
         permissions: List<String>,
-        permissionCallback: PermissionCallback,
+        permissionCallback: PermissionCallback = DefaultPermissionCallbackImpl(),
         requestMessage: String = REQUEST_MESSAGE,
         openSettingMessage: String = OPEN_SETTING_MESSAGE,
+        showDialogs: Boolean = true,
         enableLogs: Boolean = false
     ) {
         Logger.debug = enableLogs
         val weakActivity = WeakReference(activity)
-        PermissionRequestBuilder(weakActivity, null, permissions)
+        val mutablePermissionList = permissions.toMutableList()
+        PermissionRequestBuilder(weakActivity, null, mutablePermissionList)
+            .showDialogs(showDialogs)
             .setPermissionRequestMessages(requestMessage, openSettingMessage)
-            .buildAndRequest(
-                permissionCallback
-            )
+            .buildAndRequest(permissionCallback)
     }
 
+    @JvmOverloads
     fun requestPermission(
         fragment: Fragment,
         permissions: List<String>,
-        permissionCallback: PermissionCallback,
+        permissionCallback: PermissionCallback = DefaultPermissionCallbackImpl(),
         requestMessage: String = REQUEST_MESSAGE,
         openSettingMessage: String = OPEN_SETTING_MESSAGE,
+        showDialogs: Boolean = true,
         enableLogs: Boolean = false
     ) {
         Logger.debug = enableLogs
         val weakActivity = WeakReference(fragment.requireActivity())
         val weakFragment = WeakReference(fragment)
-        return PermissionRequestBuilder(
-            weakActivity,
-            weakFragment,
-            permissions
-        ).setPermissionRequestMessages(requestMessage, openSettingMessage)
+        val mutablePermissionList = permissions.toMutableList()
+        return PermissionRequestBuilder(weakActivity, weakFragment, mutablePermissionList)
+            .showDialogs(showDialogs)
+            .setPermissionRequestMessages(requestMessage, openSettingMessage)
             .buildAndRequest(permissionCallback)
     }
+
+    fun isNotificationsEnabled(context: Context): Boolean {
+        return NotificationManagerCompat.from(context).areNotificationsEnabled()
+    }
+
+    fun isPermissionGranted(context: Context, permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context, permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun isPermissionTemporarilyDenied(activity: FragmentActivity, permission: String): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)
+    }
+
+    @JvmStatic
+    fun isPermissionTemporarilyDenied(fragment: Fragment, permission: String): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(
+            fragment.requireActivity(),
+            permission
+        )
+    }
+
 }
