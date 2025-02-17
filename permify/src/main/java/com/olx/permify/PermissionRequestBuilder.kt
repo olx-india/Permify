06f8208af24f17af0f8f175594f2e25f53b31708
Abhishek Pandey <abhishek.pandey@olx.in>
2025-02-17 14:01:22 +0530

    [OLXIN-6786] : Make permission callBack mandatory

--- permify/src/main/java/com/olx/permify/PermissionRequestBuilder.kt
+++ permify/src/main/java/com/olx/permify/PermissionRequestBuilder.kt
@@ -18,6 +18,7 @@ class PermissionRequestBuilder(
     private val fragment: WeakReference<Fragment>?,
     internal val normalPermissions: MutableList<String>
 ) {
+
     init {
         filterPermission()
     }
